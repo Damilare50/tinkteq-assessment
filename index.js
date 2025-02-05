d4ea91b0b2c1321fe3663789107249f639bcb21d
Dami <damilarelamine@gmail.com>
2025-02-05 20:16:34 +0100

    chore: setup global middlewares

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import express from 'express';
 import 'dotenv/config';
 import { mongoUri } from './config/database.js';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const appPort = parseInt(process.env.PORT);
 
 // app & addons
 const app = express();
+
+// global middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
 
 // connect to db
 mongoose
